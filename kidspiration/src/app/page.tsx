3ac56acbeec6e52a82b13ef0c84e637ac8a591a0
Deebee-e <elideb4life@gmail.com>
2024-12-28 15:50:59 +0100

    added more design to the homepage

--- kidspiration/src/app/page.tsx
+++ kidspiration/src/app/page.tsx
@@ -1,18 +1,22 @@
-import Image from "next/image";
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-green-500 text-6xl font-extrabold mb-4">KIDDOMOTIVATE</h1>
-        <h1 className="mb-2 list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          {"Grooming Kids with a difference "}
-          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold text-gradient to-blue-500 to-red-500">
-            'Daily drops of Positive dew'
-          </code>
-          .
-        </h1>
+    <>
+      <Head>
+        <title>Kidspiration</title>
+        <meta name="description" content="Daily Drops of positive affirmations to inspire and motivate your kids." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${inter.className}`}>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-3xl font-bold">Welcome to Kidspiration!</h1>
+        </div>
       </main>
-    </div>
+    </>
   );
 }
